3f910886410b7cb3a1fda843a23e1767bae58abd
Mr Chibesa <chibesaobed5@gmail.com>
2025-06-18 01:22:23 +0200

    cpp simple code

---
+++ simple.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int sum = 0;
+    for(int i = 0; i < 10; i++){
+        sum += i;
+    }
+
+    cout << "sum is equal to: "<<sum<<endl;
+
+   return 0;
+}
